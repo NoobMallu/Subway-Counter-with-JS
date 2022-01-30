1047c90829a98b096c1f0cf0d942bc6dce9486e7
NoobMallu <focusyear9@gmail.com>
2022-01-30 17:49:34 +0530

    Added three new files of my project

---
+++ first.js
@@ -0,0 +1,15 @@
+//document.getElementById("count-el").innertext = 5
+
+// change the count-el in HTML to change the counting
+
+//camelCase
+
+countEl = document.getElementById("count-el")
+console.log(countEl)
+
+count = 0
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+}
